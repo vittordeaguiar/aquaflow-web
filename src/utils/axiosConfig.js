ce5797fa42f44fc1e6babd47e8e2e17c625a7a85
Vittor de Aguiar <vittoraguiar93@gmail.com>
2024-07-02 14:47:07 -0300

    ajuste URL da API

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_ROUTE || 'https://192.168.0.136:7777/v1/';
+axios.defaults.baseURL = import.meta.env.VITE_API_ROUTE || 'https://localhost:3000/v1/';
 
 try {
     axios.interceptors.request.use(
